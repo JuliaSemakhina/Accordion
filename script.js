4845108318747d462949928bd9b732d26e17f70b
JuliaSemakhina <contremoi@yandex.ru>
2023-04-14 21:21:00 +0300

    more files

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let myName = 'yuliya';
-let realName = myName.replace(myName.charAt(0), myName.charAt(0).toUpperCase());
-console.log(realName);
+const accordion = document.querySelector(".accordion");
+
+accordion.addEventListener("click", (e)=>{
+	const activePanel = e.target.closest(".accordion-panel");
+	if(!activePanel) return;
+	toggleAccordion(activePanel);
+})
+
+
+function toggleAccordion(panelToActive){
+	const buttons = panelToActive.parentElement.querySelectorAll("button");
+	const contents = panelToActive.parentElement.querySelectorAll(".accordion-content");
+	buttons.forEach((button) => {
+		button.setAttribute("aria-expanded", false)
+	});	
+	contents.forEach((content) => {
+		content.setAttribute("aria-hidden", true)
+	});
+	panelToActive.querySelector('button').setAttribute("aria-expanded", true);
+	panelToActive.querySelector('.accordion-content').setAttribute("aria-hidden", false);
+}
